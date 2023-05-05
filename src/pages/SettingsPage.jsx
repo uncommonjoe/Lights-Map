edd539b22679aef068e3d6f3a1ef32d6db05084b
Joe McLean <uncommonjoe@gmail.com>
2023-05-05 17:08:08 -0600

    Added add location button to settings page

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,21 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	TouchableOpacity,
+} from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { useNavigation } from '@react-navigation/native';
+import page from '../styles/page.style';
 
 export default function SettingsPage() {
-	return (
-		<View style={styles.container}>
-			<StatusBar style='auto' />
+	const navigation = useNavigation();
 
-			<Text>Settings</Text>
-		</View>
+	return (
+		<SafeAreaView style={page.container}>
+			<StatusBar />
+
+			<TouchableOpacity
+				style={local.listButton}
+				onPress={() => navigation.navigate('Add Location')}
+			>
+				<Text style={local.listButton.title}>Add Location</Text>
+
+				<FontAwesomeIcon
+					icon={faCirclePlus}
+					color={'black'}
+					size={22}
+					style={local.listButton.icon}
+				/>
+			</TouchableOpacity>
+		</SafeAreaView>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+const local = StyleSheet.create({
+	listButton: {
+		backgroundColor: '#f0f0f0',
+		width: '100%',
+		borderRadius: 10,
+		padding: 20,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginTop: 10,
+		title: {
+			fontSize: 18,
+			fontWeight: 'bold',
+		},
+		icon: {},
 	},
 });
